c1c33a4625641ef207d19ca715b39c130c6ed3a0
yves022 <yvesf022@gmail.com>
2026-01-25 22:08:23 +0530

    Fix: remove unsupported width prop from Skeleton usage

--- components/store/ProductCardSkeleton.tsx
+++ components/store/ProductCardSkeleton.tsx
@@ -13,19 +13,19 @@ export default function ProductCardSkeleton() {
       }}
     >
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Skeleton height={18} radius={999} width={90} />
-        <Skeleton height={18} radius={999} width={60} />
+        <Skeleton height={18} radius={999} style={{ width: 90 }} />
+        <Skeleton height={18} radius={999} style={{ width: 60 }} />
       </div>
 
       <Skeleton height={170} radius={18} />
 
       <Skeleton height={16} radius={10} />
 
-      <Skeleton height={14} radius={8} width={120} />
+      <Skeleton height={14} radius={8} style={{ width: 120 }} />
 
       <div style={{ display: "flex", gap: 8 }}>
-        <Skeleton height={40} radius={14} />
-        <Skeleton height={40} radius={14} />
+        <Skeleton height={40} radius={14} style={{ flex: 1 }} />
+        <Skeleton height={40} radius={14} style={{ flex: 1 }} />
       </div>
 
       <Skeleton height={40} radius={14} />
