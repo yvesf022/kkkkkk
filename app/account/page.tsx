8796fa08e0660bf548ef34414ec14e99381bae14
yves022 <yvesf022@gmail.com>
2026-01-28 05:04:50 +0530

    Fix TypeScript error: remove unsupported user.name from dashboard

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -37,7 +37,7 @@ export default function AccountDashboardPage() {
               color: "#0f172a",
             }}
           >
-            Hello{user?.name ? `, ${user.name}` : ""}
+            Hello
           </h1>
 
           <p
