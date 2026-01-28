e68ef5eaf0fc13e1a695bed7d16d4eb3c58ac938
yves022 <yvesf022@gmail.com>
2026-01-28 06:58:10 +0530

    Add cart and wishlist lib compatibility files

---
+++ lib/cart.ts
@@ -0,0 +1,6 @@
+"use client";
+
+export {
+  useCart,
+  type CartItem,
+} from "@/app/context/CartContext";
---
+++ lib/wishlist.ts
@@ -0,0 +1,11 @@
+"use client";
+
+export type WishlistItem = {
+  id: string;
+};
+
+export function useWishlist() {
+  throw new Error(
+    "useWishlist is not implemented yet. No wishlist logic exists in this project."
+  );
+}
